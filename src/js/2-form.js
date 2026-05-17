7eb2df9ae19262591d0086c5bc3a3a5dbd57dff5
shinobu_kage <maksim.lub@icloud.com>
2026-05-17 03:35:44 +0300

    update

---
+++ src/js/2-form.js
@@ -0,0 +1,32 @@
+const form = document.querySelector(".feedback-form");
+ const localStorageKey = "feedback-form-state";
+const textarea = form.querySelector("textarea");
+
+if(JSON.parse(localStorage.getItem(localStorageKey)) != null) {
+    const savedData = JSON.parse(localStorage.getItem(localStorageKey));
+    textarea.value = savedData.messege;
+    form.email.value = savedData.email;
+} else {
+    console.log("Fill please all fields");
+}
+
+addEventListener("input", (event) => {
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+    formData.messege = textarea.value;
+    formData.email = form.email.value;
+    console.log(formData);
+
+})
+
+
+
+addEventListener("submit", (event) => {
+    event.preventDefault();
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+})
+
+const formData ={
+    email: "",
+    messege: "",
+}
