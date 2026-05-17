37032f47e5b9ae2a2ca85962a6aba18f60d7d631
shinobu_kage <maksim.lub@icloud.com>
2026-05-17 04:21:46 +0300

    update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,35 +2,40 @@ const form = document.querySelector(".feedback-form");
  const localStorageKey = "feedback-form-state";
 const textarea = form.querySelector("textarea");
 
+const formData ={
+    email: "",
+    message: "",
+}
+
 if(JSON.parse(localStorage.getItem(localStorageKey)) != null) {
     const savedData = JSON.parse(localStorage.getItem(localStorageKey));
-    textarea.value = savedData.messege;
+    textarea.value = savedData.message;
     form.email.value = savedData.email;
 } else {
     console.log("Fill please all fields");
 }
 
-addEventListener("input", (event) => {
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
-    formData.messege = textarea.value;
+form.addEventListener("input", (event) => {
+    
+    formData.message = textarea.value;
     formData.email = form.email.value;
     console.log(formData);
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
 
 })
 
 
 
-addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => {
     if (form.email.value === "" || textarea.value === "") {
         console.log("Please fill in all the fields!");
         return;
     }
     event.preventDefault();
     localStorage.removeItem(localStorageKey);
+    formData.email = "";
+    formData.message = "";
     form.reset();
 })
 
-const formData ={
-    email: "",
-    messege: "",
-}
+    
